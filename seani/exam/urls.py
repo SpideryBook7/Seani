af3bcfa60b8b011e2e142905d85945fb82e354cf
cristian <ch374185@gmail.com>
2024-03-07 13:56:09 -0600

    07 Implements nevegation of Candidate

--- seani/exam/urls.py
+++ seani/exam/urls.py
@@ -1,1 +1,11 @@
 from django.urls import path
+
+from . import views
+
+app_name = 'exam'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('module/<int:m_id>/question/', views.question, name='question'),
+    path('module/<int:m_id>/question/', views.question, name='question'),
+    path('', views.home, name='home'),
+    ]
--- seani/templates/views.py
+++ seani/templates/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-# Create your views here.
-
 
 from .models import Exam
 from .forms import CandidateForm
+
+# Create your views here.
+def home(request):
+    user = request.user
+    return render(request, 'exam/home.html', {'user': user})
+
+def add_candidate(request):
+    if request.method == 'POST':
+        form = CandidateForm(request.POST)
+        if form.isvalid():
+            #Recibir datos
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            career = form.cleaned_data['career']
+            stage = form.cleaned_data['stage']
+            #Crear Usuario
+            user= User.objects.create_user(username, email, password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+            #Crear Examen
+            exam = Exam.objects.create(
+                user=user,
+                career=career,
+                stage=stage
+            )
+            #Llenar examen
+            exam.set_modules()
+            exam.set_questions()
+            return HttpResponse("Usuario y examen creado")
+    form = CandidateForm()
+    return render(request,
+                    'exam/add_candidate.html',
+                    {'form': form})
